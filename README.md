2cf783b61759a1bbee77c141f3920529acb4ad9a
Daniel Lemire <lemire@gmail.com>
2020-04-29 15:21:23 -0400

    Testing.

---
+++ README.md
@@ -0,0 +1,5 @@
+Simple demo to show that we can use simdjson as a CMake dependency.
+
+```
+mkdir build && cd build && cmake .. && cmake --build . && ./src/test
+```
---
+++ src/test.cpp
@@ -0,0 +1,17 @@
+#include "simdjson.h"
+int main(int argc, char* argv[]) {
+  if(argc < 2) {
+      std::cout << "provide the filename of a JSON file" << std::endl;
+      return EXIT_FAILURE;
+  }
+  try {
+    simdjson::dom::parser parser;
+    simdjson::dom::element doc = parser.load(argv[1]);
+    std::cout << "found valid JSON" << std::endl;
+    return EXIT_SUCCESS;
+  } catch (simdjson::simdjson_error &e) {
+    std::cout << "Exception! Bad JSON?" << std::endl;
+    std::cout << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+}
